4451027dd48ffa04777f4091326ba808ed40ed14
Majur Payton <dmaj@Majurs-MacBook-Pro.local>
2024-08-06 14:05:52 -0400

    Add function for and init for random word selection

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /*----- constants -----*/
 const maxGuesses = 6; // Maxium number of incorrect guesses
-
+ 
 /*----- state variables -----*/
-let wordToGuess; // Word for player to guess
+let wordToGuess = ["Nike", "Jordan", "Adidas", "Converse", "Puma", "Vans", "New Balance", "Reebok", "Saucony", "ASICS"];
 let sneaker = []; //Guessed Letters
 let incorrectGuesses = 0;
 let gameOver = false;
@@ -22,19 +22,29 @@ newGameButton.addEventListener('click', () => {
     console.log('newGameButton clicked!');
 });
 
+playButton.addEventListener('click', () => {
+    console.log('playButton clicked!');
+});
+
 keyBoardButtons.forEach(keyBoardButton => {
     keyBoardButton.addEventListener('click', () => {
         console.log('keyboardButton clicked!');
     });
-})
+});
+
+
 
 
 
 /*----- functions -----*/
-function initGame() {
-    //will add more code later
-};
+// A function to get a random word from the wordToGuess array
+function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * wordToGuess.length);
+    return wordToGuess[randomIndex];
+}
 
-function renderGameState() {
-    //will add more code later
-};
+function initGame() {
+   const randomWord = getRandomWord(); // Function to get a random word
+   console.log('Random word selected:', randomWord);
+}
+
